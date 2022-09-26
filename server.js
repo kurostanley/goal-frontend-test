8ed14bf254f149ad9eef9c09170fcbf04448dc2c
LewBlu <lewis.blundell.development@gmail.com>
2022-09-26 11:43:13 +0100

    serve app js as static item, also moved to asset folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const path = require('path');
 
 
 const app = express();
@@ -8,14 +9,15 @@ const app = express();
 app.set('view engine', 'ejs')  //使用 ejs 當作 node.js 的 view engine
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'CHANGE THIS TO FRONTEND URL'); // * allows all, or you can limit by domain
-    res.setHeader('Access-Control-Allow-Methods', '*'); // Set which header methods you want to allow (GET,POST,PUT,DELETE,OPTIONS)
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // These 2 are recommended
-    res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie'); // Required to allow the returned cookie to be set
-	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Required to allow auth credentials
+    // res.setHeader('Access-Control-Allow-Origin', 'CHANGE THIS TO FRONTEND URL'); // * allows all, or you can limit by domain
+    // res.setHeader('Access-Control-Allow-Methods', '*'); // Set which header methods you want to allow (GET,POST,PUT,DELETE,OPTIONS)
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // These 2 are recommended
+    // res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie'); // Required to allow the returned cookie to be set
+	// res.setHeader('Access-Control-Allow-Credentials', 'true'); // Required to allow auth credentials
     next();
 });
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
     res.render('index');
