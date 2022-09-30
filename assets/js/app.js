91d6378a5c0bd81620c12d04869d2e16b3f7fd8b
Shin Ting Lin <kurostanley@gmail.com>
2022-09-30 20:08:03 +0800

    workin well fontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,51 +13,31 @@ var data = {
 	"userPassword" : "1234567"
 }
 
-fetch("https://localhost:3000/api/login", {
+fetch("https://goalapi222.herokuapp.com/api/login", {
     withCredentials: true,
     credentials: 'include',
     method: "POST",
     headers: headers,
     body:  JSON.stringify(data)
 })
-.then(function(response){ 
-    return response.json(); 
-})
-.then(function(data){ 
-    console.log(data)
-});
+    .then(function(response){ 
+        return response.json(); 
+    })
+    .then(function(data){ 
+        console.log(data)
+    });
 
-// fetch("http://localhost:3000/api/user/1/goals", {
+
+// fetch("https://localhost:3000/api/login", {
+//     withCredentials: true,
 //     credentials: 'include',
-//     method: "get",
+//     method: "POST",
 //     headers: headers,
+//     body:  JSON.stringify(data)
 // })
-// .then(function(response){ 
-//     return response.json(); 
-// })
-// .then(function(data){ 
-//     console.log(data)
-// });
-
-
-// var data = JSON.stringify({
-//     "userName": "linz",
-//     "userEmail": "aab@aaa",
-//     "userPassword": "1234567"
-//   });
-  
-//   var xhr = new XMLHttpRequest();
-//   xhr.withCredentials = true;
-  
-//   xhr.addEventListener("readystatechange", function () {
-//     if (this.readyState === 4) {
-//       console.log(this.responseText);
-//     }
-//   });
-  
-//   xhr.open("POST", "http://localhost:3000/api/login");
-//   xhr.setRequestHeader("content-type", "application/json");
-//   xhr.setRequestHeader("cache-control", "no-cache");
-//   xhr.setRequestHeader("postman-token", "785c8a63-bba8-af9a-a0b0-15a6635dc9fa");
-  
-//   xhr.send(data);
+//     .then(function(response){ 
+//         return response.json(); 
+//     })
+//     .then(function(data){ 
+//         console.log(data)
+//     });
